323c31b3676928ab7c500f8da2821de1e327c12c
AnjaliVaishnav <42146367+guddu7@users.noreply.github.com>
2021-05-25 18:37:46 +0530

    api post

--- src/app/Component/register/register.component.ts
+++ src/app/Component/register/register.component.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core'; 
 import {FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { ApiService } from 'src/app/api.service';
+import { ApiService} from 'src/app/api.service';
+import { Data } from 'src/app/data.model';
 
 @Component({
   selector: 'app-register',
@@ -14,12 +15,10 @@ export class RegisterComponent{
   fname: string = "";
   lname: string= "";
   email: string="";
-  data: any=[];
-  http: any;
+  people:Data[];
+  person = new Data();
+
     constructor(private fb : FormBuilder, private api: ApiService, private httpClient: HttpClient) { 
-      this.api.getData().subscribe(data=>{
-        console.warn(data);
-      });
     this.registerForm = this.fb.group({
       fname: ['', [Validators.required, Validators.minLength(3)]],
       lname: ['', [Validators.required, Validators.minLength(3)]],
@@ -34,22 +33,48 @@ export class RegisterComponent{
  }
 
   ngOnInit(): void {
+    this.refreshData()
   }
 
-  onClick(){
-    console.warn(this.registerForm.value);
-    this.registerForm.reset();
+  refreshData() {
+    this.api.getData()
+      .subscribe(data => {
+        console.warn(data)
+        this.people=data;
+      })      
+ 
   }
-
-  submitForm() {
-    var formData: any = new FormData();
-    formData.append("fname", this.registerForm.get('fname').value);
-    formData.append("email", this.registerForm.get('email').value);
-    formData.append("password", this.registerForm.get('password').value);
-
-    this.httpClient.post<any>('http://imginfotech.in/propira/api/registration', formData).subscribe(
-      (res) => console.log(res),
-      (err) => console.log(err)
-    );
+  addData() {
+    this.api.addData(this.person)
+      .subscribe(data => {
+        console.warn(data)
+        // this.refreshData();
+      })      
+      console.warn(this.registerForm.value);
+      this.registerForm.reset();
   }
+  // onClick(){
+  //   console.warn(this.registerForm.value);
+  //   this.registerForm.reset();
+  // }
+
+  // submitForm() {
+  //   var formData: any = new FormData();
+  //   formData.append("fname", this.registerForm.get('fname').value);
+  //   formData.append("email", this.registerForm.get('email').value);
+  //   formData.append("password", this.registerForm.get('password').value);
+
+  //   this.httpClient.post<any>('http://imginfotech.in/propira/api/registration', formData).subscribe(
+  //     (res) => console.log(res),
+  //     (err) => console.log(err)
+  //   );
+  // }
+  submitForm(){
+  //   this.httpClient.post('http://imginfotech.in/propira/api/registration', this.data)
+  //   .subscribe((result)=>
+  //   console.warn("result",result)
+  //   )
+  //   console.warn(this.data);
+  }
+ 
 }
---
+++ src/app/data.model.ts
@@ -0,0 +1,6 @@
+export class Data {
+    name: string;
+    email: string;
+    password : string;
+    phonenumber : number;
+}
