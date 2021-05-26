54a56cd529f912d89db010f1abd45bf4e37fc228
AnjaliVaishnav <42146367+guddu7@users.noreply.github.com>
2021-05-26 15:19:33 +0530

    Verification

--- src/app/Component/register/register.component.ts
+++ src/app/Component/register/register.component.ts
@@ -17,13 +17,15 @@ export class RegisterComponent{
   email: string="";
   people:Data[];
   person = new Data();
+  isShown: boolean = true ;
+  show: boolean = false;
 
     constructor(private fb : FormBuilder, private api: ApiService, private httpClient: HttpClient) { 
     this.registerForm = this.fb.group({
       fname: ['', [Validators.required, Validators.minLength(3)]],
       lname: ['', [Validators.required, Validators.minLength(3)]],
       email: ['', [Validators.required, Validators.pattern('^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$')]],
-      password: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]],
       phonenumber:['', [Validators.required, Validators.minLength(10), Validators.maxLength(10), Validators.pattern('^[1-9][0-9]*$')]],
       checkbox: [false, Validators.requiredTrue],
     });
@@ -53,6 +55,8 @@ export class RegisterComponent{
       })      
       console.warn(this.registerForm.value);
       this.registerForm.reset();
+      this.isShown = !this.isShown;
+      this.show = !this.show;
   }
   // onClick(){
   //   console.warn(this.registerForm.value);
@@ -70,12 +74,8 @@ export class RegisterComponent{
   //     (err) => console.log(err)
   //   );
   // }
-  submitForm(){
-  //   this.httpClient.post('http://imginfotech.in/propira/api/registration', this.data)
-  //   .subscribe((result)=>
-  //   console.warn("result",result)
-  //   )
-  //   console.warn(this.data);
+  submit(){
+    
   }
  
 }
